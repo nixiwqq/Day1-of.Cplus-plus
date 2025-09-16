9d79022c108d5be6866e38d0325ba5ecaaac627b
Nika <129578493+nixiwqq@users.noreply.github.com>
2025-09-16 08:24:59 +0200

    Code of Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,1 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream> 
+#include <algorithm>
+using namespace std;
 
+int main () {
+    int s = 1;
+    int n = 2;
+    int t = 7;
+
+    cout<< s+n+t;
+    return 0;
+}
