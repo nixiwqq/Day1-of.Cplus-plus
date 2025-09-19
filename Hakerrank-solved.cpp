027c395bbb52210faae39c9e47293c988ccac932
Nika <129578493+nixiwqq@users.noreply.github.com>
2025-09-20 01:50:48 +0200

    Create Hakerrank-solved.cpp
    Solution for hakerrank

---
+++ Hakerrank-solved.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream> 
+#include <algorithm>
+using namespace std;
+int main() {
+    int s, n, t; cin >> s >> n >> t; cout << s + n + t;
+
+ return 0;
+}
